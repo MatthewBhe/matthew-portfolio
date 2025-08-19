df9a8aafcc9be0a4fbf566c1418c461d1eddce54
Matthew <riipper9172@gmail.com>
2025-08-18 19:26:23 -1000

    ci: deploy to GitHub Pages via Actions

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
   base: '/matthew-portfolio/',
-  build: { sourcemap: true } 
 })
